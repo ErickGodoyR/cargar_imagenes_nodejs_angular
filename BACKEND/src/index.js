9311d27253607c6a08b4740e9494eaad3c6b66e0
erick godoy <erickalexis23@hotmail.com>
2023-07-17 17:34:26 -0400

    actualización 17.07.23

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -2,20 +2,56 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
-
 
 //Middleware
 app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use(morgan('dev'));
 
+//Ruta para guardar las imagenes
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, 'uploads')
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage:storage });
+
+
 //Puerto de conexión
 app.set("port", process.env.PORT || 5000);
 
 app.get("/", (req, res) => {
     res.send("Bienvenido desde mi servidor");
+});
+
+app.post('/file', upload.single('file'), (req, res, next) => {
+    const file = req.file;
+    const filesImg = {
+        id: null,
+        nombre: file.filename,
+        imagen: file.path,
+        fecha_creacion: null
+    }
+
+    if(!file){
+        const error = new Error('No File')
+        error.httpStatusCode = 400;
+        return next(error);
+    }
+
+    res.send(file);
+    console.log(filesImg);
+
+    mysqlConnection.query('INSERT INTO files set?', [filesImg]);
+
 });
 
 
--- FRONTEND/cargarImagenes/src/app/app.component.ts
+++ FRONTEND/cargarImagenes/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cargarImagenes';
+  images = '';
+  imgUrl = 'assets/noimage.png';
+
+  constructor(private http:HttpClient){}
+  
+  ngOnInit(): void {    
+  }
+
+  selectImage(event:any){
+    if(event.target.files.length > 0){
+      const file = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+      reader.onload = (event:any) => {
+        this.imgUrl = event.target.result;
+      }
+      this.images = file;
+    }
+    
+    //console.log(event);
+  }
+
+  onSubmit(){
+    const formData = new FormData();
+    formData.append('file', this.images);
+   
+
+    this.http.post<any>('http://localhost:5000/file', formData).subscribe(
+      (res) => console.log(res,  Swal.fire({
+                icon: 'success',
+                title: 'Imagen cargada!!',
+                text: 'La imagen se subio correctamente!'
+                }).then((result) => {
+                            if (result) {
+                                       location.reload();
+                          }
+               }) 
+         ),
+      (err) => Swal.fire({
+                      icon: 'error',
+                      title: 'Oops...',
+                      text: 'Parece que no subio nada!!' 
+                    })
+    );
+   this.imgUrl = '/assets/noimage.png';  
+  }
+
+
+
+
 }
